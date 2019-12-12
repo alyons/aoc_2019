f82633797022bb5b5c1ec3e577740addd53841e1
Alexander Lyons <alexander.n.lyons@gmail.com>
2019-12-11 19:41:52 -0500

    finish day 11, mission complete!

--- days/eleven.js
+++ days/eleven.js
@@ -1,19 +1,23 @@
 const intCode = require('../src/intCode.v3');
 const program = require('../inputs/eleven.json');
 
-let currentPaintColor = 0;
+let currentPaintColor = 1;
 let input = [];
 let state = { index: 0, rIndex: 0, output: [] };
 let pIndex = 0;
 let currentRotation = 0; // 0 = UP, 1 = RIGHT, 2 = DOWN, 3 = LEFT
 let currentPosition = { x: 0, y: 0 };
 let panels = [];
+let minX = Number.MAX_SAFE_INTEGER, minY = Number.MAX_SAFE_INTEGER;
+let maxX = Number.MIN_SAFE_INTEGER, maxY = Number.MIN_SAFE_INTEGER;
 
 do {
-    input.push(currentPaintColor);
+    if (input.length == 0) { input.push(currentPaintColor); } else { input[0] = currentPaintColor; }
+    // console.log(`Input: ${input.length}`);
     state = intCode(program, input, state.output, state.index, state.rIndex);
     if (state.output.length % 2 == 0 && state.index != 'HALT') {
         let panel = { position: Object.assign({}, currentPosition), paint: state.output[pIndex] };
+        // console.log(panel);
         pIndex++;
         currentRotation += (state.output[pIndex]) ? -1 : 1;
         pIndex++;
@@ -33,10 +37,35 @@ do {
         }
         let j = panels.findIndex(p => p.position.x == currentPosition.x && p.position.y == currentPosition.y);
         currentPaintColor = (j == -1) ? 0 : panels[j].paint;
+
+        if (currentPosition.x > maxX) maxX = currentPosition.x;
+        if (currentPosition.x < minX) minX = currentPosition.x;
+        if (currentPosition.y > maxY) maxY = currentPosition.y;
+        if (currentPosition.y < minY) minY = currentPosition.y;
     }
 } while (state.index != 'HALT');
 
-console.log(state);
-console.log(state.output.length);
-console.log(pIndex);
+let canvas = '';
+
+for(let y = minY; y <= maxY; y++) {
+    for(let x = maxX; x >= minX; x--) {
+        let i = panels.findIndex(p => p.position.x == x && p.position.y == y);
+        if (i > -1) {
+            canvas += (panels[i].paint == 1) ? "#" : ' '
+        } else {
+            canvas += ' ';
+        }
+    }
+    canvas += '\n';
+}
+
+console.log(canvas);
+
+// console.log(state);
+// console.log(state.output.length);
+// console.log(pIndex);
 console.log(panels.length);
+// console.log(panels);
+// console.log(`X(${minX},${maxX}) to Y(${minY},${maxY})`);
+
+
