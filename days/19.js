147919694bc8c9ddf437a7038b0d0e9687e30e86
Alexander Lyons <alexander.n.lyons@gmail.com>
2019-12-21 07:41:12 -0500

    mission complete: 19

--- days/19.js
+++ days/19.js
@@ -4,34 +4,39 @@ const { printSameLine } = require('../src/utils');
 
 const input = [];
 let output = [];
-let maxSquare = 100;
+let maxSquare = 250;
 
-for(let x = 0; x < maxSquare; x++)  { 
-    for(let y = 0; y < maxSquare; y++) {
+for(let y = 0; y < maxSquare; y++) {
+    for(let x = 0; x < maxSquare; x++)  { 
         input.push(x);
         input.push(y);
     }
 }
 
 do {
-    state = intCode(program.slice(), input, output, 0, 0);
+    intCode(program.slice(), input, output, 0, 0);
     printSameLine(`Inputs remaining: ${input.length}`);
 } while (input.length > 0);
 console.log('\n');
-
-console.log(`Input Length: ${input.length}`);
 console.log(`Output Length: ${output.length}`);
 
 let hits = output.filter(t => t == 1);
 console.log(`Tractored Spaces: ${hits.length}`);
 
+// let toPrint = '';
 // if (maxSquare <= 100) {
 //     for(let i = 0; i < maxSquare; i++) {
-//         let toPrint = '';
-//         output.slice(i * maxSquare, (i + 1) * maxSquare ).forEach(o => toPrint += (o == 0) ? '.' : '#');
-//         console.log(toPrint);
+//         output.slice(i * maxSquare, (i + 1) * maxSquare).forEach(o => toPrint += (o == 0) ? '.' : '#');
+//     }
+// } else {
+//     for(let y = maxSquare - 100; y < maxSquare; y++) {
+//         for(let x = maxSquare - 100; x < maxSquare; x++) {
+//             toPrint += (output[y * maxSquare + x] == 0) ? '.' : '#';
+//         }
+//         toPrint += '\n';
 //     }
 // }
+// console.log(toPrint);
 
 let rightTopMost = { x: Number.MIN_SAFE_INTEGER, y: Number.MAX_SAFE_INTEGER };
 let lowerLeftMost = { x: Number.MAX_SAFE_INTEGER, y: Number.MIN_SAFE_INTEGER };
@@ -50,40 +55,48 @@ for(let y = 0; y < maxSquare; y++) {
     }
 }
 
+// console.log(`Right Top: [${rightTopMost.x}, ${rightTopMost.y}]`);
+// console.log(`Lower Left: [${lowerLeftMost.x}, ${lowerLeftMost.y}]`);
+
 const f = (x) => Math.floor((rightTopMost.y) / parseFloat(rightTopMost.x) * x);
 const g = (x) => Math.floor((lowerLeftMost.y) / parseFloat(lowerLeftMost.x) * x);
 
+let x = 0;
 let squareSize = 99;
-let xA = squareSize+ + 1;
+let xA = lowerLeftMost.x, yA = lowerLeftMost.y, xB = 100, yB = 0;
+
+console.log('Checking via formula...');
+do {
+    printSameLine(`Checking: ${x}...`);
+    if (f(x + squareSize) == g(x) - squareSize) break;
+    x++;
+} while(x < 1000);
+console.log('\n');
+
+console.log(`Expected point: [${x},${g(x)-squareSize}]`);
 
 do {
-    printSameLine(``);
-    let yA = g(xA);
-    let state = intCode(program.slice(), [xA,yA], [], 0, 0);
-    let tractor = state.output.shift();
-    if (tractor == 0) {
-        while(tractor == 0) {
-            xA++;
-            state == intCode(program.slice(), [xA,yA], [], 0, 0);
-            tractor = state.output.shift();
-        }
-    } else if (tractor == 1) {
-        while(tractor == 1) {
-            state == intCode(program.slice(), [xA - 1,yA], [], 0, 0);
-            tractor = state.output.shift();
-            if (tractor == 1) xA--;
-        }
+    let loopOutput = [];
+    intCode(program.slice(), [xA, yA], loopOutput, 0, 0);
+    let tractor = loopOutput.shift();
+    while (tractor == 0) {
+        xA++;
+        intCode(program.slice(), [xA, yA], loopOutput, 0, 0);
+        tractor = loopOutput.shift();
     }
 
-    lowerLeftMost = { x: xA, y: yA }; // Increase accuracy of g(x) with each step
-    let xB = xA + 100;
-    let yB = yA - 100;
-    state = intCode(program.slice(), [xB,yB], [], 0, 0);
-    tractor = state.output.shift();
-    if (tractor == 1) {
+    xB = xA + squareSize;
+    yB = yA - squareSize;
+
+    printSameLine(`Checking: [${xA}, ${yB}]`);
+
+    intCode(program.slice(), [xB,yB], loopOutput, 0, 0);
+    let tractorB = loopOutput.shift();
+
+    if (tractorB == 1) {
         console.log('\n');
         console.log(`Point found at ${xA, yB}`);
         break;
     }
-    xA++;
+    yA++;
 } while(true);
