a4c434e4fcbd836bb4a5de2e6320bd18d7828e5e
Alexander Lyons <alexander.n.lyons@gmail.com>
2020-01-12 11:39:28 -0500

    feat: finish day 25

--- days/25.js
+++ days/25.js
@@ -1,8 +1,10 @@
+const readlineSync = require('readline-sync');
 const intCode = require('../src/intCode.v3');
 const program = require('../inputs/25.json');
 const { printSameLine } = require('../src/utils');
 
-const NORTH = 1, SOUTH = 2, EAST = 3, WEST = 4, TAKE = 5, DROP = 6, INV = 7, EXIT = 8;
+const NORTH = 0, SOUTH = 1, EAST = 2, WEST = 3, TAKE = 4, DROP = 5, INV = 6, EXIT = -1;
+const actions = ['North', 'South', 'East', 'West', 'Take', 'Drop', 'Inventory'];
 
 let userInput;
 let input = [];
@@ -16,23 +18,53 @@ function generateInput(command, args = '') {
     let cmdString;
     switch(command) {
         case NORTH: cmdString = 'north'; break;
-        case SOUTH: cmdString = 'north'; break;
-        case EAST: cmdString = 'north'; break;
-        case WEST: cmdString = 'north'; break;
-        case TAKE: cmdString = 'north'; break;
-        case DROP: cmdString = 'north'; break;
-        default: cmdString = 'inv'; break;
+        case SOUTH: cmdString = 'south'; break;
+        case EAST: cmdString = 'east'; break;
+        case WEST: cmdString = 'west'; break;
+        case TAKE: cmdString = 'take'; break;
+        case DROP: cmdString = 'drop'; break;
+        case INV: cmdString = 'inv'; break;
     }
 
-    if (cmdString != '' && (command == TAKE || command == DROP)) cmdString += ' ' + args;
+    if (args != '' && (command == TAKE || command == DROP)) cmdString += ' ' + args;
+    // console.log(`${command} + ${args} = ${cmdString}`);
     cmdString += '\n';
 
     return cmdString.split('').map(c => c.charCodeAt(0));
 }
 
-while (state.index != 'HALT') {
-    state = intCode(program, input, state.output, state.index, state.rIndex);
-    console.log(String.fromCharCode.apply(null, state.output));
+function needCommand(value) {
+    if (value.includes('Command?')) return true;
+    if (value.includes('keypad')) return true;
+    return false;
 }
-// console.log(state);
-// console.log(program[state.index]);
+
+let breakMainLoop = false;
+
+mainLoop:
+do {
+    let breakProcessingLoop = false;
+    let toDisplay;
+    do {
+        state = intCode(program, input, state.output, state.index, state.rIndex);
+        if (state.output.find(n => n == 10)) {
+            toDisplay = String.fromCharCode.apply(null, state.output);
+            state.output = [];
+            breakProcessingLoop = true;
+        }
+    } while(!breakProcessingLoop);
+    
+
+    if (needCommand(toDisplay)) {
+        let command = 0, args = '';
+        command = readlineSync.keyInSelect(actions, 'Command?');
+        if (command == EXIT) break mainLoop;
+
+        if (command == TAKE || command == DROP) {
+            args = readlineSync.question(`What item would you like to ${(command == TAKE) ? 'take' : 'drop'}? `);
+        }
+        input = generateInput(command, args);
+    } else {
+        process.stdout.write(toDisplay);
+    }
+} while(!breakMainLoop);
